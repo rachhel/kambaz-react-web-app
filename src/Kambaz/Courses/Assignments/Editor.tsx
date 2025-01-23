b1e5d16d6919517e343be8d2faedb28f2309e669
rachhel <rachelchen1016@gmail.com>
2025-01-22 23:51:59 -0500

    first commit

---
+++ src/Kambaz/Courses/Assignments/Editor.tsx
@@ -0,0 +1,36 @@
+export default function AssignmentEditor() {
+    return (
+      <div id="wd-assignments-editor">
+        <label htmlFor="wd-name">Assignment Name</label>
+        <input id="wd-name" value="A1 - ENV + HTML" /><br /><br />
+        <textarea id="wd-description">
+          The assignment is available online Submit a link to the landing page of
+        </textarea>
+        <br />
+        <table>
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-points">Points</label>
+            </td>
+            <td>
+              <input id="wd-points" value={100} />
+            </td>
+          </tr>
+          <tr>
+          <td align="right" valign="top">
+            <label htmlFor="wd-group">Assignment Group</label>
+          </td>
+          <td>
+            <select id="wd-group">
+              <option>ASSIGNMENTS</option>
+              <option>QUIZZES</option>
+              <option>PROJECTS</option>
+            </select>
+          </td>
+        </tr>
+         
+        </table>
+      </div>
+  );}
+  
+  
---
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -0,0 +1,39 @@
+export default function Modules() {
+    return (
+      <div>
+        {/* Implement Collapse All button, View Progress button, etc. */}
+        <ul id="wd-modules">
+          <li className="wd-module">
+            <div className="wd-title">Week 1</div>
+            <ul className="wd-lessons">
+              <li className="wd-lesson">
+                <span className="wd-title">LEARNING OBJECTIVES</span>
+                <ul className="wd-content">
+                  <li className="wd-content-item">Introduction to the course</li>
+                  <li className="wd-content-item">Learn what is Web Development</li>
+                </ul>
+                <span className="wd-title">READING</span>
+                <ul className="wd-content">
+                  <li className="wd-content-item">Full Stack Developer Chapter 1</li>
+                  <li className="wd-content-item">Full Stack Developer Chapter 2</li>
+                </ul>
+                <span className="wd-title">SLIDES</span>
+                <ul className="wd-content">
+                  <li className="wd-content-item">Introduction to Web Dev</li>
+                  <li className="wd-content-item">Formatting Web Content</li>
+                </ul>
+
+              </li>
+            </ul>
+          </li>
+          <li className="wd-module">
+            <div className="wd-title">Week 2</div>
+          </li>
+          <li className="wd-module">
+            <div className="wd-title">Week 3</div>
+          </li>
+        </ul>
+      </div>
+  );}
+  
+  
