ceeed644dd7cadb45d6b05ab9327b13e185ee608
rachhel <rachelchen1016@gmail.com>
2025-02-05 23:24:54 -0500

    fixed error

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import CourseNavigation from "./Navigation";
 import Modules from "./Modules";
 import Home from "./Home";
